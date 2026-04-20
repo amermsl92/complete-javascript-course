63d77f36824cbc68db625338ebc577fdfa3d5abb
Amer Slaiman <91747941+amermsl92@users.noreply.github.com>
2026-04-20 15:14:13 +0200

    first assignemnt

---
+++ 01-Fundamentals-Part-1/starter/assignment.js
@@ -0,0 +1,9 @@
+let country = 'Sweden', continent = 'Europe', population = 1000000;
+
+console.log('The country', country, 'is in ', continent, 'with population ', population);
+
+let isIslend = true, language;
+
+console.log(typeof(isIslend))
+
+
